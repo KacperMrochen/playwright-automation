91082089d13f3822009b8fe90289485986c6513a
Mrochu <mrochen.kacper@gmail.com>
2025-08-14 22:20:41 +0200

    HomePage signup test

--- tests/test-homepage.spec.ts
+++ tests/test-homepage.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect } from "@playwright/test";
 import { TrelloHomePage } from "../pages/HomePage";
 import { TrelloLoginPage } from "../pages/LoginPage";
+import { TrelloSignupPage } from "../pages/SignupPage";
+import { TrelloWelcomePage } from "../pages/WelcomePage";
 
-test("Test trello homepage", async ({ page }) => {
+test("Test trello home login link", async ({ page }) => {
   await page.goto("/");
 
   const homePage = new TrelloHomePage(page);
@@ -13,4 +15,21 @@ test("Test trello homepage", async ({ page }) => {
   const loginPage = new TrelloLoginPage(page);
 
   await loginPage.checkLoginPageHeader();
+});
+
+test("Test trello home sign-up form", async ({ page }) => {
+  await page.goto("/");
+
+  const homePage = new TrelloHomePage(page);
+
+  await homePage.fillSignUpEmail();
+  await homePage.clickSignUpButton();
+
+  const signupPage = new TrelloSignupPage(page);
+
+  await signupPage.signupToTrello();
+
+  const welcomePage = new TrelloWelcomePage(page);
+
+  await welcomePage.checkPageHeading();
 });
