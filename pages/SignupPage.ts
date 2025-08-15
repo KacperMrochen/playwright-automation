a2c1a6bac9eaa492304c1d823919df3e6662a84b
Mrochu <mrochen.kacper@gmail.com>
2025-08-15 17:53:58 +0200

    signuppage change

--- pages/SignupPage.ts
+++ pages/SignupPage.ts
@@ -2,23 +2,27 @@ import { expect, type Locator, type Page } from "@playwright/test";
 
 export class TrelloSignupPage {
   readonly page: Page;
-  readonly formHeading: Locator;
+  readonly heading: Locator;
   readonly emailInput: Locator;
   readonly submitButton: Locator;
 
   constructor(page: Page) {
     this.page = page;
-    this.formHeading = page.locator("h1", { hasText: "Sign up to continue" });
+    this.heading = page.locator("h1", { hasText: "Sign up to continue" });
     this.emailInput = page.getByPlaceholder("Enter your email");
     this.submitButton = page.getByRole("button", { name: "Sign up" });
   }
 
   async checkSignupPageHeader() {
-    await expect(this.formHeading).toBeVisible();
+    await expect(this.heading).toBeVisible();
+  }
+
+  async checkEmailInputValue() {
+    await expect(this.emailInput).toHaveValue("example@domain.com");
   }
 
   async signupToTrello() {
-    await expect(this.emailInput).toHaveValue("example@domain.com");
+    await this.emailInput.fill("example@domain.com");
     await this.submitButton.click();
   }
 }
