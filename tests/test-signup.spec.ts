c0f978515904822b7c8b4834e9d282b6f618aa84
Mrochu <mrochen.kacper@gmail.com>
2025-08-17 19:15:11 +0200

    user agent in test

--- tests/test-signup.spec.ts
+++ tests/test-signup.spec.ts
@@ -2,6 +2,11 @@ import { test, expect } from "@playwright/test";
 import { TrelloSignupPage } from "../pages/SignupPage";
 import { TrelloLoginPage } from "../pages/LoginPage";
 
+const userAgent =
+  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36";
+
+test.use({ userAgent: userAgent });
+
 test("Test trello registered credentials signup page", async ({ page }) => {
   await page.goto("/signup");
 
