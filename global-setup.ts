370dcee00c54401bf5455ea2b141594c164ee2eb
Mrochu <mrochen.kacper@gmail.com>
2025-08-17 18:18:39 +0200

    add navigator.webdriver prop to page init script

--- global-setup.ts
+++ global-setup.ts
@@ -4,6 +4,9 @@ async function globalSetup(config: FullConfig) {
   const { baseURL, storageState } = config.projects[0].use;
   const browser = await chromium.launch();
   const page = await browser.newPage();
+  await page.addInitScript(
+    "() => Object.defineProperty(navigator,'webdriver', {get: () => undefined})"
+  );
   await page.context().storageState({ path: storageState as string });
   await browser.close();
 }
--- tests/test-signup.spec.ts
+++ tests/test-signup.spec.ts
@@ -2,19 +2,7 @@ import { test, expect } from "@playwright/test";
 import { TrelloSignupPage } from "../pages/SignupPage";
 import { TrelloLoginPage } from "../pages/LoginPage";
 
-test("Test trello registered credentials signup page", async ({
-  page,
-  context,
-}) => {
-  context.addCookies([
-    {
-      name: "sessionid",
-      value: `${Math.floor(Math.random() * 10000)}`,
-      path: "/",
-      domain: "https://trello.com",
-    },
-  ]);
-
+test("Test trello registered credentials signup page", async ({ page }) => {
   await page.goto("/signup");
 
   const signupPage = new TrelloSignupPage(page);
