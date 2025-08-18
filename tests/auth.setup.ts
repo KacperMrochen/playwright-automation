2af80fb46c283b58bfdd8c75650754ada9af41e3
Mrochu <mrochen.kacper@gmail.com>
2025-08-18 21:10:53 +0200

    debug session storage

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -6,7 +6,7 @@ import { TrelloLoginPage } from "../pages/LoginPage";
 const authFile = path.join(__dirname, "../.auth/user.json");
 
 const { LOGIN, PASSWORD } = process.env;
-const { TRELLO_LOGIN_KEY, TRELLO_LOGIN_TOKEN } = process.env;
+// const { TRELLO_LOGIN_KEY, TRELLO_LOGIN_TOKEN } = process.env;
 
 setup("Authenticate user", async ({ page }) => {
   await page.goto("/login");
@@ -15,6 +15,7 @@ setup("Authenticate user", async ({ page }) => {
 
   await loginPage.loginToTrello(LOGIN || "", PASSWORD || "");
 
+  console.log(page.context().storageState());
   await page.waitForURL("/");
   await page.context().storageState({ path: authFile });
 });
