d71c325f1847f45b18b2a85614c98446465c7771
Mrochu <mrochen.kacper@gmail.com>
2025-08-19 23:26:37 +0200

    homepage to noauth

---
+++ tests/noauth/test-homepage.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from "@playwright/test";
+import { test } from "../../fixtures";
+
+test.describe("Testing homepage", () => {
+  test("Test trello home login link", async ({ page, homePage, loginPage }) => {
+    await page.goto("/");
+
+    await homePage.isReady();
+    await homePage.clickLoginLink();
+
+    await loginPage.isReady();
+  });
+
+  test("Test trello home sign-up link", async ({
+    page,
+    homePage,
+    signupPage,
+  }) => {
+    await page.goto("/");
+
+    await homePage.fillSignUpEmail();
+    await homePage.clickSignUpButton();
+
+    await signupPage.isReady();
+    await signupPage.checkEmailInputValue();
+  });
+});
