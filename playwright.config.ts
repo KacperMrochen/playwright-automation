7d29f54182cebd56765a98bc25b931cb8db88966
Mrochu <mrochen.kacper@gmail.com>
2025-08-19 16:33:04 +0200

    cleanup

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,16 +11,11 @@ export default defineConfig({
   testDir: "./tests", // Tests dir
   fullyParallel: true, // Run tests in files in parallel
   forbidOnly: !!process.env.CI, // Fail the build on CI if you accidentally left test.only in the source code.
-  retries: 2, // Retry
+  retries: 2, // Retry times if test fails
   workers: process.env.CI ? 1 : undefined, // Opt out of parallel tests on CI.
   reporter: "html", // Reporter to use. See https://playwright.dev/docs/test-reporters
   // Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions.
   use: {
-    // extraHTTPHeaders: {
-    //   Accept: "application/json",
-    //   "Content-Type": "application/json",
-    //   Authorization: ""
-    // },
     locale: "en-GB",
     baseURL: process.env.URL, //Base URL to use in actions (switch between production and testing from .env)
     headless: process.env.HEADLESS !== "false", // Headless run mode (false local / true ci)
