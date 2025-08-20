848e580ce7fadcf17a8d316da1f6d3acca4c1074
Mrochu <mrochen.kacper@gmail.com>
2025-08-20 13:02:03 +0200

    print json file

--- tests/auth/auth.setup.ts
+++ tests/auth/auth.setup.ts
@@ -1,5 +1,6 @@
 import { test as setup } from "../../fixtures";
 import path from "path";
+import * as fs from "fs";
 
 const localAuthFile = path.join(__dirname, "../../.auth/user.json");
 
@@ -9,6 +10,12 @@ setup("Authenticate user", async ({ page, loginPage, dashboardPage }) => {
   await page.goto("/login");
 
   await loginPage.loginToTrello(LOGIN || "", PASSWORD || "");
+
+  console.log(process.env.TRELLO_PRODUCTION_AUTH);
+  fs.readFile("user.json", "utf8", function (err, data) {
+    const obj = JSON.parse(data);
+    console.log("The data from the file is: " + obj);
+  });
 
   await page.waitForURL("/");
   await page.waitForURL("/u/mrochu/boards");
